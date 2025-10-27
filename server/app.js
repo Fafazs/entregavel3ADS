08b64a405d89e3b2fa8787ba0f18ea1f08cd30e2
ALUNOD26 <alunod26@unifor.local>
2025-10-27 18:25:39 -0300

    tentando fazer com que o railway indentifique as rotas

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-
+app.use(express.static(path.join(__dirname, '..')));
 //rotas
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'index.html'));
