4fdc737a26b97b05f8c43c0efbc3f5fd7af27ac4
Fabricio Nascimento <153478556+Fafazs@users.noreply.github.com>
2025-10-27 16:21:02 -0300

    padronizando a porta para a 3000

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 3938;
+const port = 3000;
 
 // Middleware para parsear JSON
 app.use(express.json());
